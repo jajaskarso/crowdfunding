8384c79456406ad3891c8c6c79d29cf2758562af
fazaasro <fazaasro@gmail.com>
2023-01-24 09:00:04 +0700

    email checking availability

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 
 	router.Run()
 	// userInput := user.RegisterUserInput{}
