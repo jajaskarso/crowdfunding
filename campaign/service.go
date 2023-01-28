0907e3eed75df72069486814ec9b982b11ce06bf
fazaasro <fazaasro@gmail.com>
2023-01-28 20:45:24 +0700

    campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -3,6 +3,7 @@ package campaign
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -37,4 +38,24 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 		return campaign, err
 	}
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.Perks = input.Perks
+	campaign.GoalAmount = input.GoalAmount
+	campaign.User.ID = input.User.ID
+
+	//buat slug nanti
+
+	//balikan
+	newCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
 }
